120976f656f9a347c016586a52b084afb6f0beee
Sven Dickmans <30838551+dickmans@users.noreply.github.com>
2024-05-24 11:35:51 +0200

    Added capability to add existing users to groups as well with add-users.js

--- admin-helpers/add-users.js
+++ admin-helpers/add-users.js
@@ -1,4 +1,4 @@
-// ADD NEW USER TO TENANT AND ASSIGN GROUPS
+// ADD NEW USERS TO TENANT AND ASSIGN GROUPS (ALSO TO EXISTING USERS)
 // Before using this program, create a new Forge app at forge.autodesk.com (this service is for free)
 // Please provide clientId and clientSecret of your Forge app in file settings.js before 
 // Provide list of users and given groups in file /data/add-users.js
@@ -6,6 +6,7 @@
 
 /* --------------------------------------------------------------------------------------------------------
     Change Log
+    - 2024-05-24: Added capability to add existing users to groups as well
     - 2023-04-12: Adjusted to match new response of groups endpoint
     - 2022-02-16: Integration in TS F3M Extensions Package
     - 2021-03-26: Switch to Axios and manage data in external file
@@ -18,6 +19,7 @@ const utils     = require('../node_modules_adsk/utils.js');
 const data      = require('./data/add-users.js');
 
 
+let users  = [];
 let groups = [];
 let index  = 0;
 
@@ -25,10 +27,14 @@ utils.printStart([ ['User count', data.users.length] ]);
 
 f3m.login().then(function() {
     utils.print('Getting all groups');
-    f3m.getGroups().then(function(response) {
-        groups = response.items;
-        utils.print('Found ' + groups.length + ' groups in tenant');
-        addUsers();
+    f3m.getUsers().then(function(response) {
+        users = response.items;
+        utils.print('Found ' + users.length + ' users in tenant');
+        f3m.getGroups().then(function(response) {
+            groups = response.items;
+            utils.print('Found ' + groups.length + ' groups in tenant');
+            addUsers();
+        });
     });
 });
 
@@ -45,6 +51,7 @@ function addUsers() {
 function addUser() {
     
     let user = data.users[index++];
+    let link = '';
     let urns = [];
      
     console.log();
@@ -53,7 +60,14 @@ function addUser() {
     utils.print('e-Mail       : ' + user[0]);
     utils.print('Groups       : ' + user[1]);
 
-    for(group of groups) {
+    for(let account of users) {
+        if(account.email === user[0]) {
+            link = 'https://' + utils.tenant + '.autodeskplm360.net' + account.__self__;
+            break;
+        }
+    }
+
+    for(let group of groups) {
         if(user[1].indexOf(group.shortName) > -1) {
             urns.push(group.urn);
         }
@@ -61,17 +75,29 @@ function addUser() {
 
     if(urns.length === 0) {
 
+        console.log();
         utils.print('Did not find any matching group for user ' + user[0]);
+        utils.print('These groupd were defined for this user : ' + user[1]);
         utils.print('This user will be skipped');
         addUsers();
 
+    } else if(link !== '') {
+
+        utils.print('Action       : Assigning existing user to ' + urns.length + ' groups');
+
+        f3m.assignGroups(link, urns).then(function() {
+            addUsers();
+        });
+
     } else {
     
+        utils.print('Action       : Creating new user account');
+
         f3m.addUser({
             'email'      : user[0],
             'licenseCode': 'S'   // P: Participant, S: Professional
         }).then(function(response) {
-            
+
             if(response === 'error') {
                 console.log();
                 utils.print('Error occoured while adding user ' + user[0]);
