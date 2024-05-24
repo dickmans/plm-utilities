0016c99ff81920df5afdfd3428036f2db54d8313
Sven Dickmans <30838551+dickmans@users.noreply.github.com>
2024-05-24 18:41:26 +0200

    Added option for timezone and uom to add-users.js

--- admin-helpers/data/add-users.js
+++ admin-helpers/data/add-users.js
@@ -1,8 +1,12 @@
 // DATA BEING USED BY add-users.js
 // [ e-mail, [ Group1, Group2, ...] ]
 
+exports.uom      = 'Metric';     // Set to English or Metric
+exports.timezone = 'Etc/GMT+1';  // Set user timezone
 
 exports.users = [
+    ['sven.dickmans@autodesk.com', ['Engineering', 'Quality']],
     ['frank@fusion.rocks', ['Engineering', 'Quality']],
-    ['sarah.supplier@fusion.rocks', ['Supplier']]
+    ['mike@fusion.rocks', ['Suppliers']],
+    ['sarah.supplier@fusion.rocks', ['Suppliers']]
 ]
